0b50b4779e51408cd7b3a69521311837957053a9
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-15 20:07:26 +0800

    modified the code

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ for(var i=0;i<5;i++ ){
     document.write(num[i]+" ");
     sum=sum+num[i];
 }
-document.write("total sum of all number : "+sum);
+document.write("total sum of all number : "+sum +"<br/>");
 
 
 var number=new Array();
 
 for(var i=0; i<5; i++){
     number[i]=parseInt(prompt("enter a number : "));
+    document.write(number[i] + " ");
 }
 
 var total=0;
 for(var i=0; i<5; i++){
     total =total+number[i];
+
 }
+
 document.write("Total is : "+total);
